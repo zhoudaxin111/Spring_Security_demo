bca5791bf8bb004d812b78fbd751dcf04066a74c
周大信 <1507674907@qq.com>
2022-08-05 16:52:47 +0800

    idea集成Git

---
+++ src/main/java/com/gxjzy/pojo/User.java
@@ -0,0 +1,42 @@
+package com.gxjzy.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private String telephone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
