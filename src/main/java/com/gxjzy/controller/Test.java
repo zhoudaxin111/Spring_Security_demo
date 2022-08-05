c665c167a4211a809cb7b57be8383b12af9a04a6
zdx824 <72964049+zhoudaxin111@users.noreply.github.com>
2022-08-05 17:37:42 +0800

    Update Test.java

--- src/main/java/com/gxjzy/controller/Test.java
+++ src/main/java/com/gxjzy/controller/Test.java
@@ -5,6 +5,7 @@ public class Test {
         System.out.println("idea集成Git master");
         System.out.println("idea集成Git dev");
         System.out.println("push 远程提交");
+        System.out.println("pull 拉取代码")
     }
 }
 
