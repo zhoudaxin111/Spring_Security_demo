6f9e683b222f1498b706a9c64f9c1fec8f4fbe2a
周大信 <1507674907@qq.com>
2022-08-05 17:35:48 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/main/java/com/gxjzy/controller/Test.java

--- src/main/java/com/gxjzy/controller/Test.java
+++ src/main/java/com/gxjzy/controller/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("idea集成Git master");
         System.out.println("idea集成Git dev");
+        System.out.println("push 远程提交");
     }
 }
+
