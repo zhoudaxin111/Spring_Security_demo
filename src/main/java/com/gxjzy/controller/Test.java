b07f97b6d4010d853fc6c3c063e75174f7261ba6
周大信 <1507674907@qq.com>
2022-08-05 17:03:51 +0800

    idea继承Git

--- src/main/java/com/gxjzy/controller/Test.java
+++ src/main/java/com/gxjzy/controller/Test.java
@@ -2,6 +2,6 @@ package com.gxjzy.controller;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("idea集成Git");
+        System.out.println("idea集成Git dev");
     }
 }
